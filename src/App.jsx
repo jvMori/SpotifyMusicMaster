765bbf4a94081b68622848264fbbd51a34085a3c
Mori <iwona.moryc92@gmail.com>
2018-03-07 21:12:20 +0100

    app structure

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import './App.css';
+import {FormGroup, FormControl, Glyphicon, InputGroup } from 'react-bootstrap';
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+    search(){
+        console.log(this.state.query);
+    }
+    render(){
+        return (
+            <div className="App">
+                <div className="Title">Music Master</div>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl 
+                            type="text"
+                            placeholder="Search an Artist"
+                            value={this.state.query} 
+                            onChange={event => this.setState({query: event.target.value})}
+                        />
+                        <InputGroup.Addon onClick={()=> this.search()}>
+                            <Glyphicon glyph="search" />
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+                <div className="Profile">
+                    <div>Artist Picture</div>
+                    <div>Artist Name</div>
+                </div>
+                <div className="Gallery">
+                    Gallery
+                </div>
+            </div> 
+        )
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
-    <div>Music Master</div>, document.getElementById('root')
+    <App />, document.getElementById('root')
 );
